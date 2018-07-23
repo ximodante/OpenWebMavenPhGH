5b8e2d00a9e26ae6968eb21c37a272e2fb0c54f6
ximodante <ximodante@gmail.com>
2018-07-23 14:13:28 +0200

    2918.07-23 First Commit

---
+++ OpenWebMavenPhGH/src/main/java/prova/LoginBean.java
@@ -0,0 +1,30 @@
+package prova;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Named
+@SessionScoped
+public class LoginBean implements Serializable{
+ private static final long serialVersionUID = 1L;
+ @Getter @Setter private String user;
+ @Getter @Setter private String password;
+ @Getter private String myError="";
+ 
+ 
+ public String login () {
+  if (user.equalsIgnoreCase("ximo") && password.equals("password")) {
+   return "test-application-page";
+  } else {
+   myError="Bad User/password";
+   return "test-login-page";
+  }
+ }
+
+} 
